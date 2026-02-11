24aac1effd1f7f400b94efc2d0ce5851bfe6488f
fernandobritosc <fernandobritosc@gmail.com>
2026-02-11 01:34:05 -0300

    'Auto-save'

---
+++ config/taxRates.ts
@@ -0,0 +1,7 @@
+
+export const irrfRates = [
+    { value: 0.24, label: "0,24%" },
+    { value: 1.20, label: "1,20%" },
+    { value: 2.40, label: "2,40%" },
+    { value: 4.80, label: "4,80%" },
+];
