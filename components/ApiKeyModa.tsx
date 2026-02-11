94005024dae9a1d8d545698e544dc7b76e7e4ae6
fernandobritosc <fernandobritosc@gmail.com>
2026-02-11 12:31:39 -0300

    'Auto-save'

---
+++ components/ApiKeyModa.tsx
@@ -0,0 +1,3 @@
+
+// Este componente é obsoleto e não é mais utilizado.
+// A lógica do prompt de chave de API foi movida diretamente para o App.tsx para resolver um problema de importação de módulo.
