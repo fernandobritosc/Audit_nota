9b1ec2f66d293cd88759fad9afb145bdb5b2b619
fernandobritosc <fernandobritosc@gmail.com>
2026-02-11 02:46:20 -0300

    'Auto-save'

---
+++ config.ts
@@ -0,0 +1,14 @@
+
+export const irrfRates = [
+    { value: 0.24, label: "0,24%" },
+    { value: 1.20, label: "1,20%" },
+    { value: 2.40, label: "2,40%" },
+    { value: 4.80, label: "4,80%" },
+];
+
+export const issRates = [
+    { value: 2.0, label: "2,00%" },
+    { value: 3.0, label: "3,00%" },
+    { value: 4.0, label: "4,00%" },
+    { value: 5.0, label: "5,00%" },
+];
