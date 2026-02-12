5b2ed43f759ba8a56bbb0862ae2c6d388f73a7a8
fernandobritosc <fernandobritosc@gmail.com>
2026-02-11 22:02:25 -0300

    'Auto-save'

---
+++ vite-env.d.ts
@@ -0,0 +1,4 @@
+// FIX: This file is intentionally left mostly blank.
+// Global type definitions have been moved to types.d.ts to resolve conflicts
+// and remove the unnecessary reference to vite/client.
+export {};
